421e9797eacd8aab0751183ea8206e522375c42f
Roshan <84079544+Roshan45r@users.noreply.github.com>
2023-01-27 01:51:34 +0530

    Update leakybucket.py

--- lab8/leakybucket.py
+++ lab8/leakybucket.py
@@ -1,16 +1,19 @@
-no_of_queries = 4
-bucket_size = 10
-input_pkt_size = 4
-output_pkt_size = 1
-
-for i in range(0, no_of_queries):
-    size_left = bucket_size - storage
-    
-    if input_pkt_size <= size_left:
-        storage += input_pkt_size
+bucket_capacity = input("enter bucket capacity = ")
+output_rate = input("enter output rate = ")
+bucket_content = 0
+while(1):
+    packet_size = input("enter packet size = ")
+    if(packet_size > bucket_capacity):
+        print("packet size exceeded")
+        print("current bucket content = ", str(bucket_content))
+        continue
+    elif(int(packet_size)+bucket_content > int(bucket_capacity)):
+        print("bucket is already full")
+        bucket_content = bucket_content - int(output_rate)
+        print(bucket_content)
+        continue
     else:
-        print("Packet loss = ", input_pkt_size)
- 
-    print(f"Buffer size= {storage} out of bucket size = {bucket_size}")
-
-    storage -= output_pkt_size
+        bucket_content = bucket_content + int(packet_size)
+        bucket_content = bucket_content - int(output_rate)
+        print("current bucket content = "+str(bucket_content))
+    
