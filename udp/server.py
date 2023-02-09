afab5f4e3e467e7b4051b421cef8da5487c0754d
Roshan <84079544+Roshan45r@users.noreply.github.com>
2023-02-09 18:51:58 +0530

    Update server.py

--- udp/server.py
+++ udp/server.py
@@ -1,11 +1,16 @@
 from socket import *
-serverName = "127.0.0.1"
+serverName="127.0.0.1"
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-sentence = input("Enter file name")
-clientSocket.sendto(bytes(sentence,"utf-8"),(serverName, serverPort))
-filecontents,serverAddress = clientSocket.recvfrom(2048)
-print ('From Server:', filecontents)
-
-clientSocket.close()
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((serverName,serverPort))
+serverSocket.listen(1)
+while 1:
+    print ("The server is ready to receive")
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    file=open(sentence,"r")
+    l=file.read(1024)
+    connectionSocket.send(l.encode())
+    print ('\nSent contents of ' + sentence)
+    file.close()
+    connectionSocket.close()
